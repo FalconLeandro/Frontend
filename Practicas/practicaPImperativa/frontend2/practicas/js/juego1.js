68bb4fd3ff8c06fb32d889c0e46273a29fdb23ac
Leandro Falcón <cronosteo1034@gmail.com>
2022-05-10 06:03:06 -0300

    juego1

---
+++ Practicas/practicaPImperativa/frontend2/practicas/js/juego1.js
@@ -0,0 +1,44 @@
+const piedra = 1;
+const papel = 2;
+const tijera = 3; 
+let numUsuario = prompt('Elegí una opción:\n 1. Piedra\n2.Papel\n3.Tijera');
+if (numUsuario==1) {
+    alert('Elegiste Piedra')
+} else if (numUsuario==2) {
+    alert('Elegiste Papel')
+} else if (numUsuario==3) {
+    alert('Elegiste Tijera')
+};
+
+let numSecreto= parseInt(Math.random()*3+1);
+if (numSecreto==1) {
+    alert('Leandroide eligió Piedra')
+} else if (numSecreto==2) {
+    alert('Leandroide eligió Papel')
+} else if (numSecreto==3) {
+    alert('Leandroide eligió Tijera')
+}; 
+
+
+
+    if (numUsuario==1 && numSecreto==1) {
+        alert('Empate')
+    } else if (numUsuario==1 && numSecreto==2) {
+        alert('Perdiste! Papel envuelve a Piedra')
+    } else if (numUsuario==1 && numSecreto==3) {
+        alert('Ganaste! Piedra rompe Tijera')
+    } else if (numUsuario==2 && numSecreto==1) {
+        alert('Ganaste! Papel envuelve a Piedra')
+    } else if (numUsuario==2 && numSecreto==2) {
+        alert('Empate')
+    } else if (numUsuario==2 && numSecreto==3) {
+        alert('Perdiste! Tijera corta Papel')
+    } else if (numUsuario==3 && numSecreto==1) {
+        alert('Perdiste! Piedra rompe Tijera')
+    } else if (numUsuario==3 && numSecreto==2) {
+        alert('Ganaste! Tijera corta Papel')
+    } else if (numUsuario==3 && numSecreto==3) {
+        alert('Empate')
+    };
+    
+    
