01e1d225b4c6d1ca4c2ec5fa49c4653dbd0cbcac
Leandro Falcón <cronosteo1034@gmail.com>
2022-06-23 06:07:29 -0300

    matriz machine

--- Practicas/practicaPImperativa/js/matrices.js
+++ Practicas/practicaPImperativa/js/matrices.js
@@ -1,67 +1,43 @@
-// Primero vamos a crear una matriz de 2x2 con Js.
-// ¿Cómo accedo al valor almacenado en 1-1?
-// ¿Cómo puedo cambiar el valor almacenado en 1-0?
-// Probemos ahora crear matrices que no sean cuadradas, ¿como lo haríamos?
-// Sugerencia: te recomendamos que uses console.table() en vez de console.log() para poder visualizar mejor la matriz.
 
+//********MATRIZ MACHINE by Leandroide*************
 
-
-// let arrayMatriz = []
-// let arrayX=["x0","x1"]
-// let arrayY=["y0","y1"]
-// arrayMatriz[0] = arrayX
-// arrayMatriz[1] = arrayY
-// console.table(arrayMatriz)
-
-// console.log(arrayMatriz[1][1]); 
-
-// arrayMatriz[1][0]='papa';
-// console.table(arrayMatriz);
-
-//mesa de trabajo
-
-/**
- * Declara una variable que contenga una matriz
- * de 5x5 llena de puros numeros enteros y positivos
- */
-
-
+//Función para crear matriz:
 function crearMatriz(filas,columnas) {
     let matriz=new Array(filas);
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < filas; i++) {
         matriz[i]=new Array(columnas);
     }
-    console.table(matriz);
     return matriz
 }
 
-let matrizCreada = crearMatriz(12,6)
+let matrizCreada = crearMatriz(10,10) //<--Acá podés configurar la cantidad de filas y columnas de tu matriz (filas,columnas)
 
-
-function llenarMatriz(matriz) {
-    
+//Función para llenar matriz (con números enteros positivos):
+function llenarMatriz(matriz,llenadoAleatorio,rango) {
+    let acumulador = 1;
     for (let i = 0; i < matriz.length; i++) {
         for (let j = 0; j < matriz[i].length; j++) {
-            matriz[i][j]=Math.floor(Math.random()*10)
+            if (llenadoAleatorio === true) {
+                matriz[i][j]=Math.floor(Math.random()*rango)
+            } else {
+                matriz[i][j]=acumulador++
+            }
         }
     }
     console.table(matriz);
     return matriz
 }
-let matrizLlena = llenarMatriz(matrizCreada);
+let matrizLlena = llenarMatriz(matrizCreada,true,10); //<--Si querés llenado aleatorio pasale true como segundo parametro, y como tercero el rango de números. Ejemplo: llenarMatriz(array,true,10) mostrará tu array (ya definido) relleno con números aleatorios entre 1 y 10
 
-function sumarElementos(arr) {
+//Función para sumar los valores de los elementos de tu matriz:
+function sumarElementos(matriz) {
     let acumulador = 0;
-    for (let i = 0; i < arr.length; i++) {
-        acumulador += arr[i].reduce((a,b)=>a+b);
+    for (let i = 0; i < matriz.length; i++) {
+            acumulador += matriz[i].reduce((a,b)=>a+b);
     }
-    return acumulador
+    return 'Suma de los valores de todos los elementos: ' + acumulador
 }
-let totalSuma = sumarElementos(matrizLlena);
-console.log(totalSuma);
+console.log(sumarElementos(matrizLlena)); 
 
 
-/**
- * Luego, escribe un algoritmo para sumar todos
- * los numeros en la matriz.
- */
+
